efe33dbeedcc92d8704ea3c70157e5702fb6a6ca
okibe onmeje <okibeogomola@gmail.com>
2024-12-14 07:54:52 +0100

    feat(brand): add brand class to represent product brand

---
+++ backend/models/brand.js
@@ -0,0 +1,11 @@
+/**
+ * @description Represent a drug brand in the app
+ */
+
+import BaseModel from './baseModel';
+
+export default class Brand extends BaseModel {
+  constructor(brand) {
+    super(brand);
+  }
+}
