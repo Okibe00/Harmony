38b620c6ad2d45bd47f461a2dbf17810c72afa6b
okibe onmeje <okibeogomola@gmail.com>
2024-12-14 08:01:11 +0100

    feat(manufacturer) add manufacturer class for products

---
+++ backend/models/manufacturer.js
@@ -0,0 +1,11 @@
+/**
+ * @description Represent a manufacturer in the app
+ */
+
+import BaseModel from './baseModel';
+
+export default class Manufacturer extends BaseModel {
+  constructor(man) {
+    super(man);
+  }
+}
