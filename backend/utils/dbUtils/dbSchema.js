e48e2d6e7d657b6041233d5a6062c849e4f1d7c9
okibe onmeje <okibeogomola@gmail.com>
2024-12-17 01:04:57 +0100

    fix(utils): change to snake case

--- backend/utils/dbUtils/dbSchema.js
+++ backend/utils/dbUtils/dbSchema.js
@@ -4,16 +4,16 @@
 // create  database;
 export const TABLES = {
   manufacturers: `CREATE TABLE IF NOT EXISTS manufacturers (
-    manufacturer_id VARCHAR(255) PRIMARY KEY,
-    manufacturer_name VARCHAR(255) UNIQUE,
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
     country VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    INDEX idx_manufacturer_name (manufacturer_name)
+    INDEX idx_manufacturer_name (name)
 ) ENGINE=InnoDB;
 `,
   brands: `CREATE TABLE IF NOT EXISTS brands (
-    brand_id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     generic_name TEXT,
     brand_name VARCHAR(255) NOT NULL,
     manufacturer_id VARCHAR(255),
@@ -26,23 +26,23 @@ export const TABLES = {
     market_status ENUM('active', 'discontinued') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(manufacturer_id)
+    FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(id)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
     INDEX idx_brand_name (brand_name)
 ) ENGINE=InnoDB;`,
   codes: `CREATE TABLE IF NOT EXISTS codes (
-     code_id VARCHAR(255) PRIMARY KEY,
+     id VARCHAR(255) PRIMARY KEY,
      brand_id VARCHAR(255),
      manufacturer_id VARCHAR(255),
      product_code VARCHAR(100) NOT NULL UNIQUE,
      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     FOREIGN KEY (brand_id) REFERENCES brands(brand_id) ON DELETE CASCADE,
-     FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(manufacturer_id)
+     FOREIGN KEY (brand_id) REFERENCES brands(id) ON DELETE CASCADE,
+     FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(id)
 ) ENGINE=InnoDB;`,
   user: `CREATE TABLE IF NOT EXISTS users (
-    user_id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
