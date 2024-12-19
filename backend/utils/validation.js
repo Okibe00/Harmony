1754e7bb2cd34452135bd65235bc197a0695f142
okibe onmeje <okibeogomola@gmail.com>
2024-12-19 01:25:03 +0100

    feat(api): validation for brand request body

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -2,60 +2,71 @@
  * @description validation schemas
  */
 export const validateBrandDetails = {
-  manufacturer_name: {
-    notEmpty: {
-      errorMessage: 'Missing manufacturer name field',
-    },
-  },
+  // manufacturer_name: {
+  //   notEmpty: {
+  //     errorMessage: 'Missing manufacturer name field',
+  //   },
+  //   escape: true,
+  // },
   brand_name: {
     notEmpty: {
       errorMessage: 'Missing brand name field',
     },
+    escape: true,
   },
   generic_name: {
     notEmpty: {
       errorMessage: 'Missing generic name field',
     },
+    escape: true,
   },
   manufacturer_id: {
     notEmpty: {
       errorMessage: 'Missing manufacturer id field',
     },
+    escape: true,
   },
   nafdac_no: {
     notEmpty: {
       errorMessage: 'Missing nafdac no field',
     },
+    escape: true,
   },
   pack_size: {
     notEmpty: {
       errorMessage: 'Missing pack_size field',
     },
+    escape: true,
   },
   drug_class: {
     notEmpty: {
       errorMessage: 'Missing drug class field',
     },
+    escape: true,
   },
   category: {
     notEmpty: {
       errorMessage: 'Missing category field',
     },
+    escape: true,
   },
   active_ingredients: {
     notEmpty: {
       errorMessage: 'Missing active ingredients field',
     },
+    escape: true,
   },
   market_status: {
     notEmpty: {
       errorMessage: 'Missing market_status field',
     },
+    escape: true,
   },
   dosage_form: {
     notEmpty: {
       errorMessage: 'Missing dosage form field',
     },
+    escape: true,
   },
 };
 export const validateDrugDetails = {
@@ -107,37 +118,37 @@ export const validateUser = {
 export const validateManufacturer = {
   name: {
     exists: {
-      errorMessage: 'No name supplied'
+      errorMessage: 'No name supplied',
     },
     notEmpty: {
-      errorMessage: 'name cannot be empty'
+      errorMessage: 'name cannot be empty',
     },
     isLength: {
       options: {
         max: 100,
-        min: 3
+        min: 3,
       },
-      errorMessage: 'name must be between 3 and 100 characters'
+      errorMessage: 'name must be between 3 and 100 characters',
     },
-    escape: true
+    escape: true,
   },
   country: {
     exists: {
-      errorMessage: 'country must be supplied'
+      errorMessage: 'country must be supplied',
     },
     notEmpty: {
-      errorMessage: 'country cannot be empty'
+      errorMessage: 'country cannot be empty',
     },
     isLength: {
       options: {
         min: 4,
-        max: 25
+        max: 25,
       },
-    errorMessage: 'country must be between 4 and 25 characters'
+      errorMessage: 'country must be between 4 and 25 characters',
     },
-    escape: true
-  }
-}
+    escape: true,
+  },
+};
 // module.exports = {
 //   validateBrandDetails,
 //   validateDrugDetails,
