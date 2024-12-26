7fcdf85dea726dbffedab0e77e4c0eeecc4cc633
okibe onmeje <okibeogomola@gmail.com>
2024-12-27 00:33:02 +0100

    test(user): save the test user

--- backend/models/user.test.js
+++ backend/models/user.test.js
@@ -3,14 +3,16 @@
  */
 
 import Users from "./user";
-
+import { storage } from "./engine/db";
 describe('user class', () => {
-  test('should contain id, username and password field', () => {
+  test('should contain id, username and password field', async () => {
     const newUser = {
       username: "okibe007",
       password: "okibe007",
+      email: "okibe@somemail"
     }
     const okibe = new Users(newUser);
+    console.log(okibe);
     expect(okibe.id).toBeTruthy();
     expect(okibe.username).toBe('okibe007');
     expect(okibe.password).toBe("okibe007");
