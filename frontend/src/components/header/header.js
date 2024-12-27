04b179fad41718098583c621c373860cc5fcdb8c
okibe onmeje <okibeogomola@gmail.com>
2024-12-27 23:14:30 +0100

    chore(*): initiaize react app

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,17 @@
+import './header.css';
+
+export default function Header() {
+  return (
+    <nav>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </nav>
+  );
+}
