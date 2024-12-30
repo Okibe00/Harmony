1b9c83d0b2a530b1b5d0d080153b8699b183f5d3
okibe onmeje <okibeogomola@gmail.com>
2024-12-30 23:43:01 +0100

    refactor(component): remove repetitive filter operations

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -3,15 +3,15 @@
  * @param {array} data - list of filters
  */
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../Button/button';
 import './Filter.css';
 import Result from '../Result/Result';
 
-const testFilter = ['category', 'market status', 'dosage form'];
+// const testFilter = ['category', 'market status', 'dosage form'];
 
 export default function Filter() {
-  const [filter, setFilter] = useState({
+  const [searchData, setSearchData] = useState({
     query: '',
     filter: '',
   });
@@ -25,26 +25,90 @@ export default function Filter() {
       product_code: 'null',
     },
   ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/api/brands/codes/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      // console.log(response.json());
+      const data = await response.json();
+      // console.log(data);
+      setResult(data);
+    };
+    fetchData();
+  }, []);
 
   const handleChange = (e) => {
-    setFilter({ ...filter, [e.target.name]: e.target.value });
+    setSearchData({ ...searchData, [e.target.name]: e.target.value });
   };
-
-  const handleSubmit = async (e) => {
-    const response = await fetch('http://localhost:3001/api/v1/drugs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(filter),
+  const filterUtil = (state, filter) => {
+    const data = state.filter((obj) => {
+      return (
+        obj.dosage_form.toLowerCase() === filter.toLowerCase() ||
+        obj.manufacturer_name.toLowerCase() === filter.toLowerCase() ||
+        obj.drug_class.toLowerCase() === filter.toLowerCase() ||
+        obj.generic_name.toLowerCase() === filter.toLowerCase() ||
+        obj.market_status.toLowerCase() === filter.toLowerCase() ||
+        obj.category.toLowerCase() === filter.toLowerCase()
+      );
     });
-
-    if (!response.ok) {
-      throw new Error('http error ' + response.status);
-    }
-    const data = await response.json();
-    setResult(data);
+    return data;
   };
+  const handleSubmit = async (e) => {
+    const { query, filter } = searchData;
+    let filteredData = null;
+    if (!query && filter) {
+      if (result.length) {
+        filteredData = filterUtil(result, filter);
+      } else {
+        const response = await fetch(
+          'http://localhost:5000/api/brands/codes/',
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        const data = await response.json();
+        filteredData = data;
+      }
+    } else if (query && !filter) {
+      const response = await fetch('http://localhost:5000/api/brands/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ keyword: query }),
+      });
+      const data = await response.json();
+      filteredData = data;
+    } else if (query && filter) {
+      const response = await fetch('http://localhost:5000/api/brands/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ keyword: query }),
+      });
+      const data = await response.json();
+      filteredData = filterUtil(data, filter);
+    } else {
+      const response = await fetch('http://localhost:5000/api/brands/codes/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      filteredData = data;
+    }
+    setResult(filteredData);
+  };
+
   return (
     <div>
       <section className="search-section">
@@ -70,7 +134,7 @@ export default function Filter() {
             handleSubmit={handleSubmit}
           />
         </div>
-        <div className="checkbox-container">
+        {/*<div className="checkbox-container">
           {testFilter.map((value, index) => (
             <label key={index}>
               <input
@@ -82,9 +146,9 @@ export default function Filter() {
               {value}
             </label>
           ))}
-        </div>
+          </div> */}
       </section>
-      {console.log(result)}
+      {/* {console.log(result)} */}
       <Result data={result} />
     </div>
   );
