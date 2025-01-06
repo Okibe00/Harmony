e97553fda97351972dfda186412339ef38c7dbe4
okibe onmeje <okibeogomola@gmail.com>
2025-01-06 13:14:45 +0100

    feat(api): handle authenication errors and check authentication status

--- backend/api/routes/auth.js
+++ backend/api/routes/auth.js
@@ -8,11 +8,40 @@ import '../../utils/strategies/localStrategy.js';
 
 const router = Router();
 
-router.post('/login/', passport.authenticate('local'), (request, response) => {
-  console.log(request.session);
-  return response
-    .status(200)
-    .json({ status: 'Ok' });
+router.post(
+  '/login/',
+  passport.authenticate('local'),
+  (error, request, response, next) => {
+    if (error) {
+      return response.status(401).json({ authenticated: false });
+    }
+    next();
+  },
+  (request, response) => {
+    console.log(request.session);
+    return response.status(200).json({ authenticated: true });
+  }
+);
+
+router.get('/status/', (request, response) => {
+  if (request.isAuthenticated()) {
+    return response.status(200).json({ authenticated: true });
+  } else {
+    return response.status(200).json({ authenticated: false });
+  }
+});
+
+router.post('/logout/', (request, response) => {
+  if (!request.user) {
+    return response.status(401);
+  }
+  request.logOut((error) => {
+    if (!error) {
+      return response.status(200).json({ status: 'Logged out successfully' });
+    } else {
+      return response.status(400);
+    }
+  });
 });
 
 export default router;
