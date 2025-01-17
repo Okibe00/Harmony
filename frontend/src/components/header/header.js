31127a431d522d8cc89d22efce930ca4fafa31df
okibe onmeje <okibeogomola@gmail.com>
2025-01-17 22:55:31 +0100

    chore(component): remove log statement

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -13,7 +13,6 @@ export default function Header() {
   const user = useContext(loginContext);
   const [btnText, setBtnText] = useState('Login');
   const navigate = useNavigate();
-  console.log(user.isLoggedIn);
   useEffect(() => {
     if (user.isLoggedIn === true) {
       setBtnText('Logout')
